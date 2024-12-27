42f31d3cd7712fb261bcfa6bfd0f645f1c22d279
xianfu <hi.xianfu@gmail.com>
2024-12-27 16:52:11 +0800

    feat: swiper component

--- app/(tabs)/me.tsx
+++ app/(tabs)/me.tsx
@@ -1,11 +1,30 @@
 import ThemedContainer from '@/components/ThemedContainer';
-import { StyleSheet } from 'react-native';
-import { Text } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import Swiper from '@/components/Swiper';
+import { useState } from 'react';
 
 export default function TabMeScreen() {
+  const [images, setImages] = useState([
+    'https://xianfu-1321684111.cos.ap-guangzhou.myqcloud.com/image/1735287895111-swiper1.jpg',
+    'https://xianfu-1321684111.cos.ap-guangzhou.myqcloud.com/image/1735287915237-swiper2.jpg',
+    'https://xianfu-1321684111.cos.ap-guangzhou.myqcloud.com/image/1735287927105-swiper3.jpg'
+  ])
+
   return (
-    <ThemedContainer style={{ padding: 20 }}>
-      <Text variant="bodyLarge">测验</Text>
+    <ThemedContainer>
+      <Swiper images={images} />
+
+      {/* <View style={{ flexDirection: 'row', gap: 8, display: 'flex', justifyContent: 'space-between' }}>
+        <View>
+          <Text>father1</Text>
+        </View>
+        <View>
+          <Text>father2</Text>
+        </View>
+        <View>
+          <Text>father3</Text>
+        </View>
+      </View> */}
     </ThemedContainer>
   )
 }
---
+++ components/Swiper.tsx
@@ -0,0 +1,64 @@
+import { View, StyleSheet, StyleProp, ViewStyle, Image, Text } from 'react-native';
+import PagerView from 'react-native-pager-view';
+import { useState } from 'react';
+
+interface Props {
+    style?: StyleProp<ViewStyle>,
+    pagination?: boolean,
+    images: string[]
+}
+
+export default function Swiper({ style, pagination = true, images }: Props) {
+    const [currentPage, setCurrentPage] = useState(0)
+    return (
+        <View style={[styles.container, style]}>
+            <PagerView style={styles.page} initialPage={currentPage} onPageSelected={e => setCurrentPage(e.nativeEvent.position)}>
+                {
+                    images.length > 0 && images.map((image, index) => (
+                        <View key={index}>
+                            <Image source={{ uri: image }} style={styles.image} />
+                            <Text>{image}</Text>
+                        </View>
+                    ))
+                }
+            </PagerView>
+            {pagination && <View style={styles.pagination}>
+                {
+                    Array.from({ length: images.length }).map((_, index) => (
+                        <View key={index} style={[styles.paginationItem, { backgroundColor: currentPage === index ? '#000' : '#C9C9C9' }]} />
+                    ))
+                }
+            </View>}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 200,
+        marginBottom: 40,
+        position: 'relative'
+    },
+    page: {
+        height: '100%',
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    pagination: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 8,
+        marginTop: 8
+    },
+    paginationItem: {
+        width: 8,
+        height: 8,
+        borderRadius: 100
+    }
+})
+
+
